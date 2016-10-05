50126a40c181e6cd184139e811252c921441fe54
Roberto Rodriguez <prfiredragon@gmail.com>
2016-10-04 22:07:47 -0400

    Max to lend fix

--- lendingbot.py
+++ lendingbot.py
@@ -412,7 +412,7 @@ def cancelAndLoanAll():
 		if activeCur in totalLended:
 			activePlusLended += Decimal(totalLended[activeCur])
 		#log total coin
-		log.updateStatusValue(activeCur, "totalCoins", (Decimal(lendingBalances[activeCur])+Decimal(totalLended[activeCur]))
+		log.updateStatusValue(activeCur, "totalCoins", (Decimal(lendingBalances[activeCur])+Decimal(totalLended[activeCur])))
 		if loansLength == 0:
 			createLoanOffer(activeCur,Decimal(activeBal)-lent,maxDailyRate)
 		for offer in loans['offers']:
